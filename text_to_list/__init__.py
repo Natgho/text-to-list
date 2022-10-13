ee7943e0bbeeab5f7de9e77fe76b81102ce18907
Sezer Bozkir <admin@sezerbozkir.com>
2022-10-13 13:35:50 +0300

    First commit

---
+++ text_to_list/__init__.py
@@ -0,0 +1,1 @@
+from .text_to_list import text_to_list
---
+++ text_to_list/text_to_list.py
@@ -0,0 +1,7 @@
+def text_to_list(text: str):
+    """
+    :param text: The text to be taken as a string and converted to a list.
+    :return: Return parameter of function converted to list
+    """
+    listed_text = text.split(sep=" ")
+    return listed_text
